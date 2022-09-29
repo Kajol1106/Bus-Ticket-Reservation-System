29ca40c0b1044f1ccb1bca7782431da83220a9bd
Kajol1106 <kolagirkajol@gmail.com>
2022-09-29 20:52:18 +0530

    day-2 task

---
+++ src/com/Support/Menu.java
@@ -0,0 +1,18 @@
+package com.Support;
+
+public class Menu {
+	
+	public void getMenu()
+    {
+        System.out.println("---------------------------** Bus Ticket Reservation System **-----------------------------");
+        System.out.println("1.  Get status (See available buses, Arrival/Departure time, Seat Avaibility)");
+        System.out.println("2.  Book a seat ");
+        System.out.println("3.  Get travel details/detailed recipt");
+        System.out.println("4.  Exit");
+    }
+	
+//	public static void main(String[] args) {
+//		Menu m1 = new Menu();
+//		m1.getMenu();
+//	}
+}
