6fb44a9b64a3971b64f7369002828c28dc0fc205
Kajol1106 <kolagirkajol@gmail.com>
2022-09-30 20:44:45 +0530

    day-4

---
+++ src/com/Support/Demo.java
@@ -0,0 +1,13 @@
+package com.Support;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Menu m = new Menu();
+//        Charge b = new Charge();
+//        b.getBill();
+//        m.getMenu();
+	}
+
+}
